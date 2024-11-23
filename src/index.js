329f1bc7e1d0b9c314ae7398dfb6396ec6d0b0d7
Ali Mohsen <58693279+amohsen01@users.noreply.github.com>
2024-11-23 17:27:02 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,31 +9,58 @@
 import { apiUrl } from '../src/services/httpClient';
 
 class GeocoderOsm {
-    getGeoCodePosition(lat,lng){
-        let apiuri = apiUrl.osm+`&q=${lat},${lng}`;
+    getGeoCodePosition(lat, lng) {
+        let apiuri = apiUrl.osm + `&q=${lat},${lng}`;
         const requestOptions = {
-            method: 'GET'
+            method: 'GET',
+            headers: {
+                // Example headers (add actual headers if required)
+                'Accept': 'application/json',
+            }
         };
-        return fetch(`${apiuri}`, requestOptions).then((response) => {
-           return response.json()
-        }).catch((e) => {
-            console.error(e)
-            return e
-        })
+
+        // Print request headers
+        console.log("Request Headers:", Array.from(new Headers(requestOptions.headers).entries()));
+
+        return fetch(`${apiuri}`, requestOptions)
+            .then((response) => {
+                // Print all response headers
+                console.log("Response Headers:", Array.from(response.headers.entries()));
+
+                return response.json();
+            })
+            .catch((e) => {
+                console.error(e);
+                return e;
+            });
     }
 
-    getGeoAddress(place){
-        let apiuri = apiUrl.osm+`&q=${place}`;
+    getGeoAddress(place) {
+        let apiuri = apiUrl.osm + `&q=${place}`;
         const requestOptions = {
-            method: 'GET'
+            method: 'GET',
+            headers: {
+                // Example headers (add actual headers if required)
+                'Accept': 'application/json',
+            }
         };
-        return fetch(`${apiuri}`, requestOptions).then((response) => {
-           return response.json()
-        }).catch((e) => {
-            console.error(e)
-            return e
-        })
+
+        // Print request headers
+        console.log("Request Headers:", Array.from(new Headers(requestOptions.headers).entries()));
+
+        return fetch(`${apiuri}`, requestOptions)
+            .then((response) => {
+                // Print all response headers
+                console.log("Response Headers:", Array.from(response.headers.entries()));
+
+                return response.json();
+            })
+            .catch((e) => {
+                console.error(e);
+                return e;
+            });
     }
 }
+
 
 module.exports = new GeocoderOsm
